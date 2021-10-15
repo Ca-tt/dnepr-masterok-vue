3e0af6535a261c2bd0fc650a77f6ad94b0bc58fd
Alex2019Artorias <2019.alex.kim@gmail.com>
2021-10-15 18:46:56 +0300

    начали и подключили header, подключили jquery

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="">hi</button>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+    </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+</style>
