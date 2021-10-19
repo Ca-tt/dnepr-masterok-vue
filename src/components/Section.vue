df8900c751b70b3c71fd3345672fbda82fc6049a
d-doshyn <d.doshyn@gmail.com>
2021-10-19 11:44:33 +0300

    картинки и надписи над ними, кнопка вызова

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -11,11 +11,41 @@
             </div>
         </section>
         <section class="main">
-            <div class="service"></div>
-            <div class="service"></div>
-            <div class="service"></div>
-            <div class="service"></div>
-            <div class="service"></div>
+            <div class="service">
+                <img src="daniil/Vue/dnepr-masterok-vue/src/assets/q.jpg" alt="" class="service-img">
+                <h3 class="service-title">СРОЧНОЕ ВСКРЫТИЕ ДВЕРЕЙ</h3>
+                <p class="explanation-of-service">Стоимость от 400 грн<br>
+                зависит от степени сложности замка</p>
+            </div>
+            <div class="service">
+                <img src="daniil/Vue/dnepr-masterok-vue/src/assets/q.jpg" alt="" class="service-img">
+                <h3 class="service-title">СРОЧНОЕ ВСКРЫТИЕ ДВЕРЕЙ</h3>
+                <p class="explanation-of-service">Стоимость от 400 грн<br>
+                зависит от степени сложности замка</p>
+            </div>
+            <div class="service">
+                <img src="daniil/Vue/dnepr-masterok-vue/src/assets/q.jpg" alt="" class="service-img">
+                <h3 class="service-title">СРОЧНОЕ ВСКРЫТИЕ ДВЕРЕЙ</h3>
+                <p class="explanation-of-service">Стоимость от 400 грн<br>
+                зависит от степени сложности замка</p>
+            </div>
+            <div class="service">
+                <img src="daniil/Vue/dnepr-masterok-vue/src/assets/q.jpg" alt="" class="service-img">
+                <h3 class="service-title">СРОЧНОЕ ВСКРЫТИЕ ДВЕРЕЙ</h3>
+                <p class="explanation-of-service">Стоимость от 400 грн<br>
+                зависит от степени сложности замка</p>
+            </div>
+            <div class="service">
+                <img src="daniil/Vue/dnepr-masterok-vue/src/assets/q.jpg" alt="" class="service-img">
+                <h3 class="service-title">СРОЧНОЕ ВСКРЫТИЕ ДВЕРЕЙ</h3>
+                <p class="explanation-of-service">Стоимость от 400 грн<br>
+                зависит от степени сложности замка</p>
+                <a class="to-master">
+                    <figure class="give-call">
+                        <h5 class="title-of-call">СРОЧНО ВЫЗВАТЬ МАСТЕРА</h5>
+                    </figure>
+                </a>
+            </div>
         </section>
    </section>
 </template>
@@ -28,9 +58,57 @@ export default {
 </script>
 
 <style>
+    .title-of-call{
+       text-align: center;
+       color: white;
+    }
+    .give-call{
+        background-color: #fab005;
+        width: 230px;
+        height: 35px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 5px;
+    }
+    .main{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    h1, h2, h3, h4, h5, h6{
+        color: #54617a;
+    }
+    .explanation-of-service{
+        margin: 0;
+        text-align: center;
+        color: #8d99ae;
+        margin-bottom: 5px;
+    }
+    .service{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex-wrap: wrap;
+    }
+    .service-title{
+        margin-top: 10px;
+        text-align: center;
+        margin-bottom: 10px;
+        font-size: 17px;
+        color: #54617a;
+    }
+    .service-img{
+        width: 272px;
+        height: 150px;
+        margin-top: 20px;
+    }
     body{
         font-family: sans-serif;
         padding: 5%;
+        margin: 0;
+        color: #8d99ae;
     }
     div.title{
         display: flex;
@@ -41,7 +119,7 @@ export default {
     h2.title{
         font-weight: 100;
         font-family: sans-serif;
-        font-size: 45px;
+        font-size: 35px;
         margin-bottom: 10px;
         color: #54617a;
         text-align: center;
